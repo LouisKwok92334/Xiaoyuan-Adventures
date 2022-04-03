b1b677ddb66b76fba040a7834a6be81cf318739d
Louis Kwok <87352246+LouisKwok92334@users.noreply.github.com>
2022-04-04 02:29:24 +0800

    Update character.js

--- js/character.js
+++ js/character.js
@@ -10,41 +10,41 @@ const elephant = document.querySelector('.Elephant')
 
 xiaoyuan.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/Xiaoyuan.png">
+    <img src="./images/Xiaoyuan.png">
     `
 })
 boss.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/boss.png">
+    <img src="./images/boss.png">
     `
 })
 wind.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/風之精靈_希爾芙.png">
+    <img src="./images/風之精靈_希爾芙.png">
     `
 })
 fire.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/火之精靈_薩萊曼達.png">
+    <img src="./images/火之精靈_薩萊曼達.png">
     `
 })
 water.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/水之精靈_溫蒂尼.png">
+    <img src="./images/水之精靈_溫蒂尼.png">
     `
 })
 soil.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/土之精靈_諾姆.png">
+    <img src="./images/土之精靈_諾姆.png">
     `
 })
 ghost.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/ghost.png">
+    <img src="./images/ghost.png">
     `
 })
 elephant.addEventListener('click', function() {
     character.innerHTML = `
-    <img src="../images/elephant.png">
+    <img src="./images/elephant.png">
     `
 })
